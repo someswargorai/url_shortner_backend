0f38d875a5fe717bf221adcc7f0221c971065867
someswargorai <goraisomeswar@gmail.com>
2026-05-16 01:10:01 +0530

    feat: implement getUrlStatsController to aggregate and return URL click statistics

--- controller/stats.controller.js
+++ controller/stats.controller.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const modelSchema = require("../model.schema.js");
 
 const getUrlStatsController = async (req, res) => {
@@ -5,9 +6,13 @@ const getUrlStatsController = async (req, res) => {
         const { urlId } = req.params;
         const clickStateWithMinutes = await modelSchema.aggregate([
             {
-                $unwind: {
-                    path: "$countGraph"
-                }
+                $match: {
+                    shortUrl: new mongoose.Types.ObjectId(urlId),
+                    userId: new mongoose.Types.ObjectId(req.user.id)
+                } 
+            },
+            {
+                $unwind: "$countGraph"
             },
             {
                 $group: {
@@ -21,11 +26,23 @@ const getUrlStatsController = async (req, res) => {
                         $sum: "$countGraph.count"
                     }
                 }
+            },
+            {
+                $sort: {
+                    "_id": 1
+                }
             }
-        ])
-        const urlStats = await modelSchema.findOne({ _id: urlId });
+        ]);
+
+        const urlStats = await modelSchema.findOne({ shortUrl: urlId, userId: req.user.id });
+
+        if (!urlStats) {
+            return res.status(404).send({ message: "URL not found" });
+        }
+
         return res.status(200).send({ urlStats, clickStateWithMinutes });
     } catch (error) {
+        console.log(error);
         return res.status(500).send({ message: "We’re experiencing technical difficulties. Please retry after some time." });
     }
 }
